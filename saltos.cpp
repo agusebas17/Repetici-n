6a5b86c3936ec668514bf105eefb59922be8015f
agusebas17 <agusebas17@gmail.com>
2017-04-24 01:17:24 +0200

    Add files via upload

---
+++ saltos.cpp
@@ -0,0 +1,70 @@
+/*
+ * iteracion.cpp
+ *
+ *  Created on: Apr 21, 2017
+ *      Author:Agustin Sebastian
+ *      Repostiorio: agusebas17
+ */
+#include <iostream>
+
+
+
+int main() {
+	char frase [200];
+	std::cout << "Ingrese la frase que quiere que se repita:";
+	std::cin.getline(frase,200);/*Leer frase. Se duplica el comando, debido a que el primero lee el buffer*/
+	std::cout << "La frase es: "<< frase << ". Y se repetira "<<" 50 veces.\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+	std::cout << frase << "\n";
+
+
+}
